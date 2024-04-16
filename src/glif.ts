a5c81991122e0749090141644c8567153bb41e69
Jamie Dubs <jamie@jamiedubs.com>
2024-04-16 12:40:34 -0400

    OK I did it, it's the selfie bot

---
+++ src/glif.ts
@@ -0,0 +1,100 @@
+import wretch from "wretch";
+import { z } from "zod";
+
+const { DISCORD_BOT_TOKEN, DISCORD_APPLICATION_ID, GLIF_API_TOKEN } =
+  process.env;
+
+export const glifRunSchema = z.object({
+  id: z.string(),
+  inputs: z.record(z.string()).optional(),
+  output: z.string().nullable(),
+  outputFull: z.any().optional(),
+  error: z.string().optional(),
+});
+export type GlifRunSchema = z.infer<typeof glifRunSchema>;
+
+export const runGlif = async (username: string, profilePhoto: string) => {
+  if (!process.env.GLIF_API_TOKEN) throw new Error("missing GLIF_API_TOKEN");
+
+  const glifId = (await glifOfTheDay()).id;
+
+  const body = {
+    id: glifId,
+    inputs: [profilePhoto],
+  };
+
+  console.log("runGlif fetching...", body);
+  const res = await wretch("https://simple-api.glif.app")
+    .auth(`Bearer ${process.env.GLIF_API_TOKEN}`)
+    .post(body)
+    .json();
+  console.log("runGlif raw res =>", res);
+
+  const json = glifRunSchema.parse(res);
+  console.log("runGlif zod json =>", json);
+
+  if (json.error) {
+    throw new Error(json.error);
+  }
+
+  return json.output;
+};
+
+const glifUrls = [
+  [
+    "Genius Wojak Yourself",
+    "https://glif.app/@fab1an/glifs/clta3pqie000gr5sjwtmdsa2d",
+  ],
+  [
+    "Wojak Yourself",
+    "https://glif.app/@fab1an/glifs/clt5mu59o000rff7hgotv5dxl",
+  ],
+  [
+    "Instant 90s Yearbook Yourself",
+    "https://glif.app/@fab1an/glifs/clrg8yj3w0002ew574bno9ull",
+  ],
+  [
+    "Santa Anime Yourself",
+    "https://glif.app/@fab1an/glifs/clqdnkyd0001egy4wew187mbg",
+  ],
+  [
+    "Enhanced Twitter PFPs",
+    "https://glif.app/@fab1an/glifs/clq2iyfnn001ddwfuxp2kuqs6",
+  ],
+  [
+    "RoastGPT: Roast Your Selfie",
+    "https://glif.app/@fab1an/glifs/clo5gzr31000kmj0f2xeqjb25",
+  ],
+  [
+    "Face Redesigner (tiger)",
+    "https://glif.app/@fab1an/glifs/clm7pfbo60002jt0fjldvivrp",
+  ],
+  [
+    "Impression Selfie",
+    "https://glif.app/@fab1an/glifs/clptup7q100051031mjolo8kb",
+  ],
+  [
+    "80s Fantasy Figure of Yourself",
+    "https://glif.app/@fab1an/glifs/clqnsn20x0030qbm790rm3kc4",
+  ],
+];
+
+export const glifs = glifUrls.map(([name, url]) => {
+  const split = url.split("/");
+  const username = split[3].replace("@", "");
+  return {
+    id: split[5],
+    name,
+    url,
+    username,
+    userUrl: `https://glif.app/@${username}`,
+  };
+});
+
+const randomGlif = () => {
+  return glifs[Math.floor(Math.random() * glifs.length)];
+};
+
+export const glifOfTheDay = async () => {
+  return randomGlif();
+};
--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,18 @@ import {
   CommandInteraction,
   SlashCommandBuilder,
   PartialMessageReaction,
+  TextChannel,
+  PartialUser,
+  PartialMessage,
 } from "discord.js";
+import { runGlif, glifOfTheDay, glifs } from "./glif";
 
 dotenv.config();
 
-const { DISCORD_BOT_TOKEN, DISCORD_APPLICATION_ID } = process.env;
-
+const { DISCORD_BOT_TOKEN, DISCORD_APPLICATION_ID, GLIF_API_TOKEN } =
+  process.env;
 if (!DISCORD_BOT_TOKEN || !DISCORD_APPLICATION_ID) {
   throw new Error("missing required ENV vars");
-}
-
-const IMAGE_FILE = "path/to/image.png";
-
-// Replace the function that generates the image
-async function runGlif(
-  username: string,
-  userProfilePhoto: string
-): Promise<string> {
-  return "path/to/generated/image.png";
-}
-
-export const data = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Replies with Pong!");
-
-export async function execute(interaction: CommandInteraction) {
-  return interaction.reply("Pong!");
 }
 
 const client = new Client({
@@ -42,14 +28,26 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.DirectMessageReactions,
+    // GatewayIntentBits.DirectMessages,
+    // GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.MessageContent,
   ],
 });
+
+client.login(DISCORD_BOT_TOKEN);
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}!`);
 });
+
+// TODO need to register slash commands
+// export const data = new SlashCommandBuilder()
+//   .setName("ping")
+//   .setDescription("Replies with Pong!");
+//
+// export async function execute(interaction: CommandInteraction) {
+//   return interaction.reply("Pong!");
+// }
 
 client.on("messageCreate", async (message: Message) => {
   if (message.author.bot) {
@@ -57,44 +55,114 @@ client.on("messageCreate", async (message: Message) => {
     return;
   }
 
-  if (message.content === "!image") {
-    const imageEmbed = new EmbedBuilder()
-      .setImage("attachment://image.png")
-      .setTimestamp();
+  console.log(message);
 
-    const sentMessage = await message.channel.send({
-      embeds: [imageEmbed],
-      files: [IMAGE_FILE],
-    });
+  const data = {
+    content: message.content,
+    channel: {
+      type: message.channel.type,
+      name:
+        message.channel instanceof TextChannel ? message.channel.name : "???",
+    },
+    author: {
+      username: message.author.username,
+      displayName: message.author.displayName,
+      globalName: message.author.globalName,
+      avatar: message.author.displayAvatarURL(),
+    },
+  };
+  console.log("messageCreate", data);
 
-    await sentMessage.react("🖼️");
+  // if (message.content === "!image") {
+  //   const imageEmbed = new EmbedBuilder()
+  //     .setImage("attachment://image.png")
+  //     .setTimestamp();
+
+  //   const sentMessage = await message.channel.send({
+  //     embeds: [imageEmbed],
+  //     files: [IMAGE_FILE],
+  //   });
+
+  //   await sentMessage.react("🖼️");
+  // }
+
+  if (message.content === "!selfie") {
+    processMessage(message, message.author);
+  } else if (message.content === "!glifs") {
+    await message.channel.send(
+      "Available selfie glifs:\n" +
+        glifs.map((glif) => `- [${glif.name}](${glif.url})`).join("\n")
+    );
+  } else if (message.content == "!help") {
+    await message.channel.send(`Commands:
+- !selfie
+- !glifs
+- !help`);
   }
 });
 
 client.on("messageReactionAdd", async (reaction, user) => {
   if (user.bot) return;
-
   if (reaction.emoji.name === "🖼️") {
-    const message = reaction.message;
-    const processingMessage = await message.channel.send(
-      `Processing for ${user.username}...`
-    );
-
-    const userProfilePhoto = user.displayAvatarURL();
-    const generatedImagePath = await runGlif(user.username, userProfilePhoto);
-
-    await processingMessage.delete();
-
-    const generatedImageEmbed = new EmbedBuilder()
-      .setImage("attachment://generated_image.png")
-      .setTimestamp();
-
-    await message.channel.send({
-      content: `<@${user.id}>`,
-      embeds: [generatedImageEmbed],
-      files: [generatedImagePath],
-    });
+    processMessage(reaction.message, user);
   }
 });
 
-client.login(DISCORD_BOT_TOKEN);
+async function processMessage(
+  message: Message | PartialMessage,
+  user: User | PartialUser
+) {
+  // abort if partial (for now)
+  if (!user?.username) {
+    console.log("processMessage: missing data, skipping", { message, user });
+    return;
+  }
+
+  const glif = await glifOfTheDay();
+
+  // discord seems to be largely capped around 358x358
+  const userProfilePhoto = user.displayAvatarURL({ size: 1024 });
+
+  const addonMessages = [
+    "you look nice today btw",
+    "did you get a haircut?",
+    "i like your style",
+    "so cute",
+    "reticulating splines (lol)",
+    "nice photo you got there",
+    "doo doo dee doo",
+    "this will take a sec",
+    "this won't take long",
+    "omg look out behind you! just kidding",
+  ];
+  const addonMessage =
+    addonMessages[Math.floor(Math.random() * addonMessages.length)];
+
+  const processingMessage = await message.channel.send(`Today's glif: [${
+    glif.name
+  }](${glif.url}) by [@${glif.username}](${glif.userUrl})
+Processing [${user.username}](${userProfilePhoto})... ${
+    addonMessage ? `${addonMessage}...` : ""
+  }`);
+
+  let imageUrl;
+  try {
+    imageUrl = await runGlif(user.username, userProfilePhoto);
+  } catch (e) {
+    console.error("runGlif error", e);
+    await processingMessage.edit("Error processing image, sorry :(");
+    return;
+  }
+
+  await processingMessage.delete();
+
+  // const generatedImageEmbed = new EmbedBuilder()
+  //   .setImage(imageUrl)
+  //   .setTimestamp();
+
+  await message.channel.send({
+    content: `<@${user.id}> run through [${glif.name}](${glif.url})`,
+    files: [imageUrl],
+    // embeds: [generatedImageEmbed],
+  });
+}
