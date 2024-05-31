53c81aaf0310bbc56ad3dfed50e3d970e90df16a
Jamie Dubs <jamie@jamiedubs.com>
2024-05-31 14:17:39 -0400

    Add tons of new glifs and make it truly 'glif of the day'

--- src/glif.ts
+++ src/glif.ts
@@ -13,7 +13,11 @@ export const glifRunSchema = z.object({
 });
 export type GlifRunSchema = z.infer<typeof glifRunSchema>;
 
-export const runGlif = async (id: string, username: string, profilePhoto: string) => {
+export const runGlif = async (
+  id: string,
+  username: string,
+  profilePhoto: string
+) => {
   if (!process.env.GLIF_API_TOKEN) throw new Error("missing GLIF_API_TOKEN");
 
   const body = {
@@ -40,14 +44,6 @@ export const runGlif = async (id: string, username: string, profilePhoto: string
 
 const glifUrls = [
   [
-    "Genius Wojak Yourself",
-    "https://glif.app/@fab1an/glifs/clta3pqie000gr5sjwtmdsa2d",
-  ],
-  [
-    "Wojak Yourself",
-    "https://glif.app/@fab1an/glifs/clt5mu59o000rff7hgotv5dxl",
-  ],
-  [
     "Instant 90s Yearbook Yourself",
     "https://glif.app/@fab1an/glifs/clrg8yj3w0002ew574bno9ull",
   ],
@@ -63,14 +59,6 @@ const glifUrls = [
     "RoastGPT: Roast Your Selfie",
     "https://glif.app/@fab1an/glifs/clo5gzr31000kmj0f2xeqjb25",
   ],
-  /*[
-    "Face Redesigner (tiger)",
-    "https://glif.app/@fab1an/glifs/clm7pfbo60002jt0fjldvivrp",
-  ],*/
-  /*[
-    "Impression Selfie",
-    "https://glif.app/@fab1an/glifs/clptup7q100051031mjolo8kb",
-  ],*/
   [
     "80s Fantasy Figure of Yourself",
     "https://glif.app/@fab1an/glifs/clqnsn20x0030qbm790rm3kc4",
@@ -83,6 +71,62 @@ const glifUrls = [
     "Potato Jesus Yourself",
     "https://glif.app/@Heather/glifs/clw9sm11r0000tvp100lytxde",
   ],
+  [
+    "GlifStation 1 Covert Art",
+    "https://glif.app/@fab1an/glifs/clue1e2z50000ysvqtnet2ywx",
+  ],
+  [
+    "Statue-fy Anyone",
+    "https://glif.app/@Ell3n/glifs/clt9vzip900005tzi4gfzujm5",
+  ],
+
+  //all below use text input (for glifmoji), not image URL
+  // TODO remix them and swithc to iam image input if possible!
+  // [
+  //   "GymFakes",
+  //   "https://glif.app/@fab1an/glifs/clmfyd5r60004kx0flw80eeoa",
+  // ],
+  // [
+  //   "Steve Jobs Iconic Pose",
+  //   "https://glif.app/@Carlos/glifs/cllnu4kk6000lky088psr1ann",
+  // ],
+  // [
+  //   "life is hard",
+  //   "https://glif.app/@dham/glifs/cllcp43om0000jm097ztpzxh3",
+  // ],
+  // [
+  //   "Pixar Yourself",
+  //   "https://glif.app/@fab1an/glifs/cll4zlk200000kw08z6vvmm5v",
+  // ],
+  // [
+  //   "Bob Ross Paintings",
+  //   "https://glif.app/@fab1an/glifs/cll3g8yu70000l70814w2v4pa",
+  // ],
+  // [
+  //   "Claymation Style",
+  //   "https://glif.app/@fab1an/glifs/cll1zrmc4000alb084bjyho31",
+  // ],
+  // [
+  //   "Rustpunk Yourself",
+  //   "https://glif.app/@fab1an/glifs/cll0okucb000kmj08ry2zicu5",
+  // ],
+  // [
+  //   "Anime Yourself",
+  //   "https://glif.app/@fab1an/glifs/cll0m847a000ejt08g9f2b7j2",
+  // ],
+  // [
+  //   "Historical Selfies",
+  //   "https://glif.app/@fab1an/glifs/cll0k0hgd0000mn08waxpo9hg",
+  // ],
+  // [
+  //   // FIXME multiple inputs
+  //   "Barbie Yourself",
+  //   "https://glif.app/@fab1an/glifs/clkzskx4d0004l708lfampw2l",
+  // ],
+  // [
+  //   "Oppenheimer Yourself",
+  //   "https://glif.app/@fab1an/glifs/clkivyj22000ikz08vqmep7cb",
+  // ]
 ];
 
 export const glifs = glifUrls.map(([name, url]) => {
@@ -102,5 +146,8 @@ const randomGlif = () => {
 };
 
 export const glifOfTheDay = async () => {
-  return randomGlif();
+  const date = new Date();
+  const datestamp = date.toISOString().split("T")[0];
+  const index = Number(datestamp.split("-").join("")) % glifs.length;
+  return glifs[index];
 };
